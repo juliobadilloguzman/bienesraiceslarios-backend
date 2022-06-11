31994d09fc5c53fb5ec9f5760d700113c3c50b29
“Julio” <juliobadilloguzman@gmail.com>
2022-06-11 17:49:01 -0500

    Cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const fs = require('fs');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create(AppModule, {cors: true});
+
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(AppModule.port);
